d2443327acde5ba763c898d3524ad361a944ab4b
redpois0n <red@cmail.nu>
2015-05-16 10:45:33 +0200

    Client Plugin

---
+++ Plugin Window Controller Client/src/se/jrat/plugin/window/client/WindowPlugin.java
@@ -0,0 +1,11 @@
+package se.jrat.plugin.window.client;
+
+import jrat.api.Plugin;
+
+public class WindowPlugin extends Plugin {
+
+	public WindowPlugin() {
+		super("Window Controller", "1.0", "Window Controller Plugin", "jRAT");
+	}
+
+}
