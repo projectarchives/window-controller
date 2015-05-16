8e8761a8d87776c9679d8df0a4f7b74dd7ec2cc8
redpois0n <red@cmail.nu>
2015-05-16 11:35:31 +0200

    WindowPanel, icon, RATControlMenuEntry

--- Plugin Window Controller Client/src/se/jrat/plugin/window/client/WindowPlugin.java
+++ Plugin Window Controller Client/src/se/jrat/plugin/window/client/WindowPlugin.java
@@ -1,7 +1,9 @@
 package se.jrat.plugin.window.client;
 
+import iconlib.IconUtils;
 import jrat.api.Plugin;
 import jrat.api.net.Packet;
+import jrat.api.ui.RATControlMenuEntry;
 
 public class WindowPlugin extends Plugin {
 	
@@ -10,6 +12,9 @@ public class WindowPlugin extends Plugin {
 	public WindowPlugin() {
 		super("Window Controller", "1.0", "Window Controller Plugin", "jRAT");
 		
+		RATControlMenuEntry entry = new RATControlMenuEntry("Windows", IconUtils.getIcon("windows", WindowPlugin.class), WindowPanel.class);		
+		RATControlMenuEntry.addEntry(entry);
+
 		Packet.registerIncoming(HEADER_LIST, new ListPacketListener());
 	}
 
