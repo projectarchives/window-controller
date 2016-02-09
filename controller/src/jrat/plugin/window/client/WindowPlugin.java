a6ee42e659ffb521787bd82d27a3163a5504d170
redpois0n <red@cmail.nu>
2016-02-09 10:15:02 +0100

    Fix icon

--- controller/src/jrat/plugin/window/client/WindowPlugin.java
+++ controller/src/jrat/plugin/window/client/WindowPlugin.java
@@ -1,17 +1,24 @@
 package jrat.plugin.window.client;
 
-import iconlib.IconUtils;
 import jrat.api.Plugin;
 import jrat.api.net.Packet;
 import jrat.api.ui.RATControlMenuEntry;
+
+import javax.swing.ImageIcon;
 
 public class WindowPlugin extends Plugin {
 	
 	public static final short HEADER_LIST = -127;
 
 	public WindowPlugin() {
-		super("Window Controller", "1.0", "Window Controller Plugin", "jRAT", IconUtils.getIcon("windows", WindowPlugin.class));
-		
+		super("Window Controller", "1.0", "Window Controller Plugin", "jRAT");
+
+		try {
+			icon = new ImageIcon(getResource("/icons/windows.png"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		RATControlMenuEntry entry = new RATControlMenuEntry("Windows", icon, WindowPanel.class);		
 		RATControlMenuEntry.addEntry(entry);
 
